5ef664c16a00a54094878c5287627ff7567abde6
dcostakos <david.costakos@redhat.com>
2015-08-26 12:19:12 -0700

    Adding ceph zap disk script

--- scripts/zap_ceph_disk.sh
+++ scripts/zap_ceph_disk.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+echo "Killing Ceph"
+systemctl stop ceph || /bin/true
+pkill ceph || /bin/true
+systemctl | grep ceph | awk '{ print $1 }' | xargs -n1 systemctl stop || /bin/true
+echo "DONE"
+
 DEV=$1
 while [ "${DEV}" != "" ] ; do
-  partprobe ${DEV}
-  sgdisk --zap-all ${DEV}
+  echo "On ${DEV}"
+  mount -a | egrep "^${DEV}" | awk '{ print $3 }' | xargs -n 1 umount
+  ceph-disk zap ${DEV}
   partprobe ${DEV}
   ceph-disk zap ${DEV}
+  gdisk ${DEV}
   shift
   DEV=$1
 done
