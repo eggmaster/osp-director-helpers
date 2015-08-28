0e044e484d524164c42c7dfd866c5806355a8014
dcostakos <david.costakos@redhat.com>
2015-08-28 10:03:57 -0700

    Fixing zap_ceph_disk.sh

--- scripts/zap_ceph_disk.sh
+++ scripts/zap_ceph_disk.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 echo "Killing Ceph"
-systemctl stop ceph || /bin/true
-pkill ceph || /bin/true
-systemctl | grep ceph | awk '{ print $1 }' | xargs -n1 systemctl stop || /bin/true
-mount | grep ceph | awk '{ print $3 }' | xargs -n 1 umount
+  systemctl stop ceph || /bin/true
+  pkill ceph-osd || /bin/true
+  systemctl | grep ceph | awk '{ print $1 }' | xargs -n1 systemctl stop || /bin/true
+  mount | grep ceph | awk '{ print $3 }' | xargs -n 1 umount || /bin/true
 echo "DONE"
+
+ZAP_COMMAND="ceph-disk zap"
+
+if ! which ceph-disk > /dev/null 2>&1 ; then
+  ZAP_COMMAND="sgdisk -Zog"
+fi
+
+echo "Zap Command is $ZAP_COMMAND"
 
 DEV=$1
 while [ "${DEV}" != "" ] ; do
   echo "On ${DEV}"
-  ceph-disk zap ${DEV}
+  ${ZAP_COMAND} ${DEV}
   partprobe ${DEV}
   shift
   DEV=$1
 done
-  
+
