a8e8b2a7916f4d1da86c4e6311303459deb6fe6d
Steve Linabery <slinaber@redhat.com>
2015-12-02 10:05:13 -0600

    Use subprocess.check_output
    Some of the commands using subprocess.call were silently failing.
    Define a cmd method that allows us to see the output and also will
    error if the child process exits non-zero.

--- overcloud_heat_examples/sahara/sahara-install.py
+++ overcloud_heat_examples/sahara/sahara-install.py
@@ -7,20 +7,23 @@ def install_sahara():
  sahara_pass = "sahara"
  sahara_ip = "192.168.1.10"
 
- subprocess.call(["yum", "-y", "install", "openstack-sahara-api", "openstack-sahara-engine"])
- subprocess.call(["mysql", "-u", "root","-e","create database sahara; grant all on sahara.* to 'sahara'@'%' identified by 'sahara';grant all on sahara.* to 'sahara'@'"+sahara_ip+"' identified by 'sahara';"])
- subprocess.call(["openstack-config", "--set", "/etc/sahara/sahara.conf", "database", "connection", "mysql://sahara:sahara@"+sahara_ip+"/sahara"])
- subprocess.call(["sahara-db-manage", "--config-file", "/etc/sahara/sahara.conf", "upgrade", "head"])
- subprocess.call(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "auth_uri", "http://"+sahara_ip+":5000/v2.0/"])
- subprocess.call(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "identity_uri", "http://"+sahara_ip+":35357"])
- subprocess.call(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "admin_tenant_name", "service"])
- subprocess.call(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "admin_user", "sahara"])
- subprocess.call(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "admin_password", "sahara"])
- subprocess.call(["openstack-config", "--set", "/etc/sahara/sahara.conf", "DEFAULT", "use_neutron", "true"])
- subprocess.call(["systemctl", "start", "openstack-sahara-api.service"])
- subprocess.call(["systemctl", "start", "openstack-sahara-engine.service"])
- subprocess.call(["systemctl", "enable", "openstack-sahara-api.service"])
- subprocess.call(["systemctl", "enable", "openstack-sahara-engine.service"])
+ print cmd(["yum", "-y", "install", "openstack-sahara-api", "openstack-sahara-engine"])
+ print cmd(["mysql", "-u", "root","-e","create database sahara; grant all on sahara.* to 'sahara'@'%' identified by 'sahara';grant all on sahara.* to 'sahara'@'"+sahara_ip+"' identified by 'sahara';"])
+ print cmd(["openstack-config", "--set", "/etc/sahara/sahara.conf", "database", "connection", "mysql://sahara:sahara@"+sahara_ip+"/sahara"])
+ print cmd(["sahara-db-manage", "--config-file", "/etc/sahara/sahara.conf", "upgrade", "head"])
+ print cmd(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "auth_uri", "http://"+sahara_ip+":5000/v2.0/"])
+ print cmd(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "identity_uri", "http://"+sahara_ip+":35357"])
+ print cmd(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "admin_tenant_name", "service"])
+ print cmd(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "admin_user", "sahara"])
+ print cmd(["openstack-config", "--set", "/etc/sahara/sahara.conf", "keystone_authtoken", "admin_password", "sahara"])
+ print cmd(["openstack-config", "--set", "/etc/sahara/sahara.conf", "DEFAULT", "use_neutron", "true"])
+ print cmd(["systemctl", "start", "openstack-sahara-api.service"])
+ print cmd(["systemctl", "start", "openstack-sahara-engine.service"])
+ print cmd(["systemctl", "enable", "openstack-sahara-api.service"])
+ print cmd(["systemctl", "enable", "openstack-sahara-engine.service"])
+
+def cmd(args):
+    return subprocess.check_output(args)
 
 def main():
  hostname = socket.gethostname()
