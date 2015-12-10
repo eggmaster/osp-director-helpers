a9f8b6d5e059779dc6691877ef70efe591761d72
Steve Linabery <slinaber@redhat.com>
2015-12-10 14:57:23 -0600

    Make sahara prep script more parameterized

--- overcloud_heat_examples/sahara/sahara-prep-script.sh
+++ overcloud_heat_examples/sahara/sahara-prep-script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#SET THESE:
+INTERNAL_VIP=
+EXTERNAL_VIP=
+
 source /home/stack/overcloudrc
 keystone user-create --name sahara --pass sahara
 keystone user-role-add --user sahara --role admin --tenant service
 keystone service-create --name sahara --type data-processing --description "OpenStack Data Processing"
-keystone endpoint-create --service sahara --publicurl 'http://127.0.0.1:8386/v1.1/%(tenant_id)s' --adminurl 'http://127.0.0.1:8386/v1.1/%(tenant_id)s' --internalurl 'http://127.0.0.1:8386/v1.1/%(tenant_id)s'
+keystone endpoint-create --service sahara --publicurl 'http://${EXTERNAL_VIP}:8386/v1.1/%(tenant_id)s' --adminurl 'http://${INTERNAL_VIP}:8386/v1.1/%(tenant_id)s' --internalurl 'http://${INTERNAL_VIP}:8386/v1.1/%(tenant_id)s'
